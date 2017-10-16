3758f30aa5ae9c28dac445b5095b3bac8b10a3aa
ReactiioN1337 <reactiion@haze-productions.com>
2017-10-16 13:47:43 +0200

    :alien: updated timer api

--- resource/timer.lua
+++ resource/timer.lua
@@ -7,17 +7,20 @@ Timer.startTime  = nil
 Timer.finished   = false
 Timer.duration   = nil
 Timer.percentage = 0
+Timer.callback   = nil
 
-function Timer.new(duration)
+function Timer.new(duration, callback)
   local timer = deepcopy(Timer)
-  if type(duration) == 'number' then
-    timer:setDuration(duration)
-  end
+  timer:setDuration(duration)
+  timer:setCallback(callback)
   return timer
 end
 
-function Timer:run()
+function Timer:run(arg)
   if self.finished then
+    if type(self.callback) == 'function' then
+      self.callback(arg)
+    end
     return true
   end
 
@@ -52,4 +55,10 @@ end
 function Timer:reset()
   self.startTime = nil
   self.finished  = false
+end
+
+function Timer:setCallback(callback)
+  if type(callback) == 'function' then
+    self.callback = callback
+  end
 end
